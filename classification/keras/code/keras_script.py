79194a651944b8e08c4251b2317c18140ede0608
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2023-01-06 13:50:58 +0100

    Addressed review comments

--- classification/keras/code/keras_script.py
+++ classification/keras/code/keras_script.py
@@ -6,23 +6,23 @@
 
 # ## Setup
 
-# In[41]:
+# In[1]:
 
 
 import os
 
-import numpy as np
 import tensorflow as tf
+import utils
 
 # (Neptune) Import Neptune and initialize a project
 
-# In[42]:
+# In[2]:
 
 
 os.environ["NEPTUNE_PROJECT"] = "showcase/project-text-classification"
 
 
-# In[43]:
+# In[3]:
 
 
 import neptune.new as neptune
@@ -54,85 +54,68 @@ print("Downloading data...")
 project["data/keras/files"].download("..")
 
 
-# ### Extract downloaded files
+# ### Prepare data
 
 # In[6]:
 
 
-import tarfile
-
-print("Extracting data...")
-my_tar = tarfile.open("../aclImdb_v1.tar.gz")
-my_tar.extractall("..")
-my_tar.close()
-
-
-# ### Remove `unsup` subfolder and rename `aclImdb` folder
-
-# In[7]:
-
-
-import shutil
-
-shutil.rmtree("../aclImdb/train/unsup")
-os.remove("../aclImdb_v1.tar.gz")
-
-if os.path.exists("../data"):
-    shutil.rmtree("../data")
-os.rename("../aclImdb", "../data")
+utils.extract_files(source="../aclImdb_v1.tar.gz", destination="..")
+utils.prep_data(imdb_folder="../aclImdb", dest_path="../data")
 
 
 # (Neptune) Upload dataset sample to Neptune project
 
-# In[8]:
+# In[7]:
 
 
 import random
 
-project["data/keras/sample/train/pos"].upload(
+base_namespace = "data/keras/sample/"
+
+project[base_namespace]["train/pos"].upload(
     f"../data/train/pos/{random.choice(os.listdir('../data/train/pos'))}"
 )
-project["data/keras/sample/train/neg"].upload(
+project[base_namespace]["train/neg"].upload(
     f"../data/train/neg/{random.choice(os.listdir('../data/train/neg'))}"
 )
-project["data/keras/sample/test/pos"].upload(
+project[base_namespace]["test/pos"].upload(
     f"../data/test/pos/{random.choice(os.listdir('../data/test/pos'))}"
 )
-project["data/keras/sample/test/neg"].upload(
+project[base_namespace]["test/neg"].upload(
     f"../data/test/neg/{random.choice(os.listdir('../data/test/neg'))}"
 )
 
 
 # ### Generate training, validation, and test datasets
 
-# In[44]:
+# In[8]:
 
 
 data_params = {
     "batch_size": 32,
     "validation_split": 0.2,
-    "max_features": 20000,
+    "max_features": 2000,
     "embedding_dim": 128,
     "sequence_length": 500,
-    "seed": 42,
+    "seed": 1,
 }
 
 
 # (Neptune) Log data metadata to Neptune
 
-# In[45]:
+# In[9]:
 
 
 run = neptune.init_run(name="Keras text classification", tags=["keras"])
 
 
-# In[46]:
+# In[10]:
 
 
 run["data/params"] = data_params
 
 
-# In[47]:
+# In[11]:
 
 
 import re
@@ -140,7 +123,7 @@ import string
 
 from tensorflow.keras.layers import TextVectorization
 
-# In[48]:
+# In[12]:
 
 
 raw_train_ds, raw_val_ds = tf.keras.preprocessing.text_dataset_from_directory(
@@ -160,20 +143,9 @@ print(f"Number of batches in raw_val_ds: {raw_val_ds.cardinality()}")
 print(f"Number of batches in raw_test_ds: {raw_test_ds.cardinality()}")
 
 
-# ### Previewing data
-
-# In[49]:
-
-
-for text_batch, label_batch in raw_train_ds.take(1):
-    for i in range(5):
-        print(text_batch.numpy()[i])
-        print(label_batch.numpy()[i])
-
-
 # ### Clean data
 
-# In[50]:
+# In[13]:
 
 
 def custom_standardization(input_data):
@@ -195,7 +167,7 @@ vectorize_layer.adapt(text_ds)
 
 # ### Vectorize data
 
-# In[51]:
+# In[14]:
 
 
 def vectorize_text(text, label):
@@ -218,7 +190,7 @@ test_ds = test_ds.cache().prefetch(buffer_size=10)
 
 # (Neptune) Create a new model and model version
 
-# In[52]:
+# In[15]:
 
 
 from neptune.new.exceptions import NeptuneModelKeyAlreadyExistsError
@@ -228,14 +200,16 @@ project_key = project.get_structure()["sys"]["id"].fetch()
 try:
     model = neptune.init_model(name="keras", key="KER")
     model.stop()
-    model_version = neptune.init_model_version(model=f"{project_key}-KER", name="keras")
 except NeptuneModelKeyAlreadyExistsError:
-    model_version = neptune.init_model_version(model=f"{project_key}-KER", name="keras")
+    # If it already exists, we don't have to do anything.
+    pass
+
+model_version = neptune.init_model_version(model=f"{project_key}-KER", name="keras")
 
 
 # ### Build a model
 
-# In[53]:
+# In[16]:
 
 
 model_params = {
@@ -249,62 +223,23 @@ model_params = {
 }
 
 
-# In[54]:
+# In[17]:
 
 
 model_version["params"] = model_params
 
 
-# In[55]:
+# In[18]:
 
 
-from tensorflow.keras import layers
-
-# A integer input for vocab indices.
-inputs = tf.keras.Input(shape=(None,), dtype="int64")
-
-# Next, we add a layer to map those vocab indices into a space of dimensionality
-# 'embedding_dim'.
-x = layers.Embedding(data_params["max_features"], data_params["embedding_dim"])(inputs)
-x = layers.Dropout(model_params["dropout"])(x)
-
-# Conv1D + global max pooling
-x = layers.Conv1D(
-    data_params["embedding_dim"],
-    model_params["kernel_size"],
-    padding="valid",
-    activation=model_params["activation"],
-    strides=model_params["strides"],
-)(x)
-x = layers.Conv1D(
-    data_params["embedding_dim"],
-    model_params["kernel_size"],
-    padding="valid",
-    activation=model_params["activation"],
-    strides=model_params["strides"],
-)(x)
-x = layers.GlobalMaxPooling1D()(x)
-
-# We add a vanilla hidden layer:
-x = layers.Dense(data_params["embedding_dim"], activation=model_params["activation"])(x)
-x = layers.Dropout(model_params["dropout"])(x)
-
-# We project onto a single unit output layer, and squash it with a sigmoid:
-predictions = layers.Dense(1, activation="sigmoid", name="predictions")(x)
-
-keras_model = tf.keras.Model(inputs, predictions)
-
-# Compile the model with binary crossentropy loss and an adam optimizer.
-keras_model.compile(
-    loss=model_params["loss"], optimizer=model_params["optimizer"], metrics=model_params["metrics"]
-)
+keras_model = utils.build_model(model_params, data_params)
 
 
 # ### Train the model
 
 # (Neptune) Initialize the Neptune callback
 
-# In[56]:
+# In[19]:
 
 
 from neptune.new.integrations.tensorflow_keras import NeptuneCallback
@@ -312,7 +247,7 @@ from neptune.new.integrations.tensorflow_keras import NeptuneCallback
 neptune_callback = NeptuneCallback(run=run, log_model_diagram=True, log_on_batch=True)
 
 
-# In[57]:
+# In[20]:
 
 
 training_params = {
@@ -320,27 +255,30 @@ training_params = {
 }
 
 
-# In[58]:
+# In[21]:
 
 
 # Fit the model using the train and test datasets.
 keras_model.fit(
-    train_ds, validation_data=val_ds, epochs=training_params["epochs"], callbacks=neptune_callback
+    train_ds,
+    validation_data=val_ds,
+    epochs=training_params["epochs"],
+    callbacks=neptune_callback,
 )
 # Training parameters are logged automatically to Neptune
 
 
 # ### Evaluate the model
 
-# In[59]:
+# In[22]:
 
 
-_, acc = keras_model.evaluate(test_ds, callbacks=neptune_callback)
+_, curr_model_acc = keras_model.evaluate(test_ds, callbacks=neptune_callback)
 
 
 # ## (Neptune) Associate run with model and vice-versa
 
-# In[60]:
+# In[23]:
 
 
 run_meta = {
@@ -348,16 +286,17 @@ run_meta = {
     "name": run.get_structure()["sys"]["name"].fetch(),
     "url": run.get_url(),
 }
-run_meta
+
+print(run_meta)
 
 
-# In[61]:
+# In[24]:
 
 
 model_version["run"] = run_meta
 
 
-# In[62]:
+# In[25]:
 
 
 model_version_meta = {
@@ -365,10 +304,11 @@ model_version_meta = {
     "name": model_version.get_structure()["sys"]["name"].fetch(),
     "url": model_version.get_url(),
 }
-model_version_meta
+
+print(model_version_meta)
 
 
-# In[63]:
+# In[26]:
 
 
 run["training/model/meta"] = model_version_meta
@@ -376,13 +316,13 @@ run["training/model/meta"] = model_version_meta
 
 # ## (Neptune) Upload serialized model and model weights to Neptune
 
-# In[64]:
+# In[27]:
 
 
 model_version["serialized_model"] = keras_model.to_json()
 
 
-# In[65]:
+# In[28]:
 
 
 keras_model.save_weights("model_weights.h5")
@@ -391,7 +331,7 @@ model_version["model_weights"].upload("model_weights.h5")
 
 # (Neptune) Wait for all operations to sync with Neptune servers
 
-# In[66]:
+# In[29]:
 
 
 model_version.sync()
@@ -401,16 +341,14 @@ model_version.sync()
 
 # ### (Neptune) Fetch current production model
 
-# In[67]:
+# In[30]:
 
 
-model = neptune.init_model(with_id=f"{project_key}-KER")
-model_versions_df = model.fetch_model_versions_table().to_pandas()
-model.stop()
-model_versions_df
+with neptune.init_model(with_id=f"{project_key}-KER") as model:
+    model_versions_df = model.fetch_model_versions_table().to_pandas()
 
 
-# In[68]:
+# In[31]:
 
 
 production_models = model_versions_df[model_versions_df["sys/stage"] == "production"]["sys/id"]
@@ -419,64 +357,52 @@ assert (
 ), f"Multiple model versions found in production: {production_models.values}"
 
 
-# In[69]:
+# In[32]:
 
 
 prod_model_id = production_models.values[0]
 print(f"Current model in production: {prod_model_id}")
 
 
-# In[70]:
+# In[33]:
 
 
-prod_model = neptune.init_model_version(with_id=prod_model_id)
-prod_model_params = prod_model["params"].fetch()
-loaded_prod_model = tf.keras.models.model_from_json(
-    prod_model["serialized_model"].fetch(), custom_objects=None
+npt_prod_model = neptune.init_model_version(with_id=prod_model_id)
+npt_prod_model_params = npt_prod_model["params"].fetch()
+prod_model = tf.keras.models.model_from_json(
+    npt_prod_model["serialized_model"].fetch(), custom_objects=None
 )
 
-prod_model["model_weights"].download()
-loaded_prod_model.load_weights("model_weights.h5")
+npt_prod_model["model_weights"].download()
+prod_model.load_weights("model_weights.h5")
 
 
 # ### (Neptune) Evaluate current model on lastest test data
 
-# In[71]:
+# In[34]:
 
 
 # using the model's original loss and optimizer, but the current metric
-loaded_prod_model.compile(
-    loss=prod_model_params["loss"],
-    optimizer=prod_model_params["optimizer"],
+prod_model.compile(
+    loss=npt_prod_model_params["loss"],
+    optimizer=npt_prod_model_params["optimizer"],
     metrics=model_params["metrics"],
 )
 
-_, prod_model_acc = loaded_prod_model.evaluate(test_ds)
+_, prod_model_acc = prod_model.evaluate(test_ds)
 
 
 # ### (Neptune) If challenger model outperforms production model, promote it to production
 
-# In[72]:
+# In[36]:
 
 
-print(f"Production model accuracy: {prod_model_acc}\nChallenger model accuracy: {acc}")
+print(f"Production model accuracy: {prod_model_acc}\nChallenger model accuracy: {curr_model_acc}")
 
-if acc > prod_model_acc:
+if curr_model_acc > prod_model_acc:
     print("Promoting challenger to production")
-    prod_model.change_stage("archived")
+    npt_prod_model.change_stage("archived")
     model_version.change_stage("production")
 else:
     print("Archiving challenger model")
     model_version.change_stage("archived")
-
-prod_model.stop()
-
-
-# ## (Neptune) Stop tracking
-
-# In[73]:
-
-
-model_version.stop()
-run.stop()
-project.stop()
